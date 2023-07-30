5b88c0cccc9a0004c56503be0e7d2bd382b5d8d1
Parvez Shaikh 2 <parvezshaikh.0894@gmail.com>
2023-07-30 17:54:50 +0530

    - Initialized Retrofit client.

--- app/src/main/java/com/example/swipedemo/utils/RestAPIClass.kt
+++ app/src/main/java/com/example/swipedemo/utils/RestAPIClass.kt
@@ -4,19 +4,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RestAPIClass {
+    private lateinit var retrofit: Retrofit
+    private val baseUrl = "https://app.getswipe.in/api/public/"
 
-    companion object {
-        private lateinit var retrofit: Retrofit
-        private const val baseUrl = "https://app.getswipe.in/api/public/"
-        fun getClient(): Retrofit {
-            retrofit = Retrofit
-                .Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
-            return retrofit
-        }
+    fun getClient(): Retrofit {
+        retrofit = Retrofit
+            .Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit
     }
 
 }
