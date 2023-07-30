ebbd4e95dcea909f9e8369df129a3379d23a97ef
Parvez Shaikh 2 <parvezshaikh.0894@gmail.com>
2023-07-30 17:54:13 +0530

    - Removed unused import.

--- app/src/main/java/com/example/swipedemo/product_listing/utils/ProductListingViewModel.kt
+++ app/src/main/java/com/example/swipedemo/product_listing/utils/ProductListingViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.swipedemo.product_listing.models.ProductListingResponseModel
 import com.example.swipedemo.product_listing.models.ProductListingResponseModelItem
 import com.example.swipedemo.utils.GenericResponse
 import kotlinx.coroutines.Dispatchers
